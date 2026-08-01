f92b85eba7c4c77de40c65f8e4635396b430a3d7
Andres Mena <andresmena284@gmail.com>
2026-08-01 18:53:27 -0500

    feat: Se agrego la validacion de los campos de Login y Register

--- src/features/auth/pages/login/LoginPage.tsx
+++ src/features/auth/pages/login/LoginPage.tsx
@@ -1,60 +1,93 @@
-import { Link } from "react-router";
+
+import React, { useState } from 'react'
 
 export const LoginPage = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState({ email: '', password: '' })
+
+
+  const validate = () => {
+    const newErrors = { email: '', password: '' }
+
+    if (!email) {
+      newErrors.email = 'El Correo es requerido'
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      newErrors.email = 'El Correo no tiene un formato valido'
+    }
+
+    if (!password) {
+      newErrors.password = 'La contraseña es Requerida'
+    } else if (password.length < 8) {
+      newErrors.password = 'La contraseña debe tener mínimo 8 caracteres'
+    }
+
+    setErrors(newErrors)
+
+    return !newErrors.email && !newErrors.password
+  }
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault()
+    validate()
+  }
+
+
   return (
-    <div className="flex min-h-[calc(100vh-64px)] items-center justify-center bg-gradient-to-b from-black via-zinc-900 to-black px-6">
-      <div className="w-full max-w-md rounded-2xl border border-white/10 bg-zinc-900/80 p-8 shadow-2xl backdrop-blur">
-        <h1 className="text-center text-3xl font-bold text-white">
-          Iniciar Sesión
+    <div className="flex-1 min-h-screen flex items-center justify-center bg-gradient-to-br from-black via-red-950 to-black">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-mauve-600/40 p-8 flex flex-col rounded-2xl shadow-xl w-full max-w-sm border border-blue-900">
+        <h1 className="text-amber-50 text-2xl font-bold mb-6 text-center">
+          Bienvenido a tu cine favorito
         </h1>
 
-        <p className="mt-2 text-center text-gray-400">
-          Bienvenido de nuevo.
-        </p>
 
-        <form className="mt-8 space-y-5">
+
+        <div className="flex flex-col space-y-4">
           <div>
-            <label className="mb-2 block text-sm text-gray-300">
-              Correo electrónico
-            </label>
-
             <input
               type="email"
-              placeholder="correo@ejemplo.com"
-              className="w-full rounded-lg border border-zinc-700 bg-zinc-800 px-4 py-3 text-white outline-none transition focus:border-red-500"
+              name='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="bg-white px-4 py-2 rounded-lg text-slate-900 outline-none focus:ring-2 focus:ring-amber-400 placeholder-slate-400 w-full"
+              placeholder="Correo electrónico"
             />
+            {errors.email && (
+              <p className="text-red-400 text-sm mt-1">{errors.email}</p>
+            )}
           </div>
+
 
           <div>
-            <label className="mb-2 block text-sm text-gray-300">
-              Contraseña
-            </label>
-
             <input
               type="password"
-              placeholder="********"
-              className="w-full rounded-lg border border-zinc-700 bg-zinc-800 px-4 py-3 text-white outline-none transition focus:border-red-500"
+              name='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="bg-white px-4 py-2 rounded-lg text-slate-900 outline-none focus:ring-2 focus:ring-amber-400 placeholder-slate-400 w-full"
+              placeholder="contraseña"
             />
+            {errors.password && (
+              <p className="text-red-400 text-sm mt-1">{errors.password}</p>
+            )}
           </div>
 
-          <button
-            type="submit"
-            className="w-full rounded-lg bg-red-600 py-3 font-semibold transition hover:bg-red-700"
-          >
-            Iniciar Sesión
-          </button>
-        </form>
 
-        <p className="mt-6 text-center text-gray-400">
-          ¿No tienes cuenta?{" "}
-          <Link
-            to="/register"
-            className="font-semibold text-red-500 hover:underline"
-          >
-            Regístrate
-          </Link>
-        </p>
-      </div>
+        </div>
+
+        <button
+
+          type="submit"
+          className="bg-amber-50 hover:bg-amber-100 text-blue-950 font-semibold py-2 rounded-lg mt-6 transition-colors duration-200"
+        >
+          Iniciar sesión
+        </button>
+      </form>
     </div>
-  );
-};
+
+  )
+}
+export default LoginPage
--- src/features/auth/pages/register/RegisterPage.tsx
+++ src/features/auth/pages/register/RegisterPage.tsx
@@ -1,6 +1,50 @@
+import { useState } from "react";
 import { Link } from "react-router";
 
 export const RegisterPage = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [errors, setErrors] = useState({ name: '', email: '', password: '', confirmPassword: '' })
+
+  const validate = () => {
+    const newErrors = { name: '', email: '', password: '', confirmPassword: '' }
+
+    if (!name) {
+      newErrors.name = 'El nombre es requerido'
+    }
+
+    if (!email) {
+      newErrors.email = 'El correo es requerido'
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      newErrors.email = 'El correo no tiene un formato válido'
+    }
+
+    if (!password) {
+      newErrors.password = 'La contraseña es Requerida'
+    } else if (password.length < 8) {
+      newErrors.password = 'La contraseña debe tener mínimo 8 caracteres'
+    }
+
+    if (!confirmPassword) {
+      newErrors.confirmPassword = 'La contraseña es Requerida'
+    } else if (password !== confirmPassword) {
+      newErrors.confirmPassword = 'La contraseñas no coinciden'
+    }
+
+    setErrors(newErrors)
+
+    return !newErrors.name && !newErrors.email && !newErrors.password && !newErrors.confirmPassword
+  }
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault()
+    validate()
+  }
+
+
+
   return (
     <div className="flex min-h-[calc(100vh-64px)] items-center justify-center bg-gradient-to-b from-black via-zinc-900 to-black px-6">
       <div className="w-full max-w-md rounded-2xl border border-white/10 bg-zinc-900/80 p-8 shadow-2xl backdrop-blur">
@@ -12,7 +56,7 @@ export const RegisterPage = () => {
           Únete a nuestra plataforma.
         </p>
 
-        <form className="mt-8 space-y-5">
+        <form onSubmit={handleSubmit} className="mt-8 space-y-5">
           <div>
             <label className="mb-2 block text-sm text-gray-300">
               Nombre
@@ -21,8 +65,13 @@ export const RegisterPage = () => {
             <input
               type="text"
               placeholder="Juan Pérez"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="w-full rounded-lg border border-zinc-700 bg-zinc-800 px-4 py-3 text-white outline-none transition focus:border-red-500"
             />
+            {errors.name && (
+              <p className="text-red-400 text-sm mt-1">{errors.name}</p>
+            )}
           </div>
 
           <div>
@@ -33,8 +82,13 @@ export const RegisterPage = () => {
             <input
               type="email"
               placeholder="correo@ejemplo.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="w-full rounded-lg border border-zinc-700 bg-zinc-800 px-4 py-3 text-white outline-none transition focus:border-red-500"
             />
+            {errors.email && (
+              <p className="text-red-400 text-sm mt-1">{errors.email}</p>
+            )}
           </div>
 
           <div>
@@ -45,8 +99,13 @@ export const RegisterPage = () => {
             <input
               type="password"
               placeholder="********"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="w-full rounded-lg border border-zinc-700 bg-zinc-800 px-4 py-3 text-white outline-none transition focus:border-red-500"
             />
+            {errors.password && (
+              <p className="text-red-400 text-sm mt-1">{errors.password}</p>
+            )}
           </div>
 
           <div>
@@ -57,8 +116,13 @@ export const RegisterPage = () => {
             <input
               type="password"
               placeholder="********"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
               className="w-full rounded-lg border border-zinc-700 bg-zinc-800 px-4 py-3 text-white outline-none transition focus:border-red-500"
             />
+            {errors.confirmPassword && (
+              <p className="text-red-400 text-sm mt-1">{errors.confirmPassword}</p>
+            )}
           </div>
 
           <button
